8207a052064bc2518995d217e721d43128222eb1
codewithashith <codewithashith@gmail.com>
2023-07-21 10:35:44 +0530

    01. Redux toolkit setup

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ import App from "./App";
 // npm install -D tailwindcss
 // npx tailwindcss init
 
+// Redux: The core library enables the redux architecture.
+// React Redux: Simplifies connecting your React components to the Redux store.
+// React Toolkit: Intended to be the standard way to write Redux logic.
+// Redux DevTools Extension: Connects your Redux application to Redux DevTools
+//
+// npm install redux react-redux redux-devtools-extension @reduxjs/toolkit
+
+// 1. Create Store with empty reducer
+// 2. Add redux provider and connect to the store in Index.ts
+// 3. Create slice with initialState and return reducer
+// 4. Import the reducer in store and add it to the reducer.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
